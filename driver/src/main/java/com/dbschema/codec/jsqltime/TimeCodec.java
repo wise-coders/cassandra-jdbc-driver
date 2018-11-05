de2826a8e2bed181f364eba4dbfd718e38a23096
Liudmila Kornilova <liudmila.kornilova@jetbrains.com>
2018-11-05 18:51:25 +0300

    Fix TypeCodec for time. Store time as number of nanoseconds

--- driver/src/main/java/com/dbschema/codec/jsqltime/TimeCodec.java
+++ driver/src/main/java/com/dbschema/codec/jsqltime/TimeCodec.java
@@ -1,8 +1,6 @@
 package com.dbschema.codec.jsqltime;
 
-import com.datastax.driver.core.DataType;
-import com.datastax.driver.core.ProtocolVersion;
-import com.datastax.driver.core.TypeCodec;
+import com.datastax.driver.core.*;
 import com.datastax.driver.core.exceptions.InvalidTypeException;
 
 import java.nio.ByteBuffer;
@@ -13,6 +11,7 @@ import java.sql.Time;
  **/
 public class TimeCodec extends TypeCodec<java.sql.Time> {
     public static final TimeCodec INSTANCE = new TimeCodec();
+    private final TypeCodec<Long> timeCodec = CodecRegistry.DEFAULT_INSTANCE.codecFor(DataType.time(), Long.class);
 
     private TimeCodec() {
         super(DataType.time(), java.sql.Time.class);
@@ -21,15 +20,15 @@ public class TimeCodec extends TypeCodec<java.sql.Time> {
     @Override
     public ByteBuffer serialize(Time value, ProtocolVersion protocolVersion) throws InvalidTypeException {
         if (value == null) return null;
-        ByteBuffer bb = ByteBuffer.allocate(8);
-        bb.putLong(0, value.getTime());
-        return bb;
+        long milliseconds = value.getTime();
+        return timeCodec.serialize(milliseconds * 1000000, protocolVersion);
     }
 
     @Override
     public Time deserialize(ByteBuffer bytes, ProtocolVersion protocolVersion) throws InvalidTypeException {
         if (bytes == null) return null;
-        return new Time(bytes.getLong());
+        long nanoseconds = timeCodec.deserialize(bytes, protocolVersion);
+        return new Time(nanoseconds / 1000000);
     }
 
     @Override
