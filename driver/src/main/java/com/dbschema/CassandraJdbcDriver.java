b9948d76f3320394efa02c25563615f829ed346c
Liudmila Kornilova <liudmila.kornilova@jetbrains.com>
2019-02-11 15:38:46 +0300

    Version 1.3

--- driver/src/main/java/com/dbschema/CassandraJdbcDriver.java
+++ driver/src/main/java/com/dbschema/CassandraJdbcDriver.java
@@ -110,7 +110,7 @@ public class CassandraJdbcDriver implements Driver {
     }
 
     String getVersion() {
-        return "1.2.9";
+        return "1.3";
     }
 
     @Override
@@ -120,7 +120,7 @@ public class CassandraJdbcDriver implements Driver {
 
     @Override
     public int getMinorVersion() {
-        return 29;
+        return 3;
     }
 
     @Override
